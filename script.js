2a3b50dfe4dc99fdd320e8741246142964e9e0f9
shahriar nafis <snafaro02@gmail.com>
2025-09-03 10:26:33 +0600

    basic animation created

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const h1 = document.querySelector("h1");
-const h1Text = h1.textContent;
-const splitText = h1Text.split("");
-let spanText = "";
+function textSplit(){
+    const h1 = document.querySelector("h1");
+    const h1Text = h1.textContent;
+    const splitText = h1Text.split("");
+    let spanText = "";
 
-splitText.forEach((item)=>{
-    spanText += `<span>${item}</span>`;
-});
+    splitText.forEach((item)=>{
+        spanText += `<span>${item}</span>`;
+    });
+    h1.innerHTML = spanText;
+}
 
-h1.innerHTML = spanText;
-console.log(spanText) 
+textSplit();
+
+gsap.from("h1 span",{
+    y:50,
+    opacity: 0,
+    duration: .5,
+    stagger: .1
+})
+
