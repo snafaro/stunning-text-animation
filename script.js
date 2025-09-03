20b64fe1fe84ddcf37c4f204b63f616c7138f0c0
shahriar nafis <snafaro02@gmail.com>
2025-09-03 10:18:38 +0600

    split text and put every character inside span

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("animated text")
+const h1 = document.querySelector("h1");
+const h1Text = h1.textContent;
+const splitText = h1Text.split("");
+let spanText = "";
+
+splitText.forEach((item)=>{
+    spanText += `<span>${item}</span>`;
+});
+
+h1.innerHTML = spanText;
+console.log(spanText) 
